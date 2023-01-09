8c5834c9061caf2789a5cf6aca32da6cb99664b6
ayaan ali <ayaanali5204@outlook.com>
2023-01-09 12:05:03 -0500

    Day 1
    First Day HTML dev

---
+++ components/Home.vue
@@ -0,0 +1,19 @@
+<template> 
+<div>
+<center>
+<h1>Hi! My name is Ayaan </h1>
+<img 
+<h4> I'm a student at Columbia University studying computer science and mathematics. </h4> 
+	<img src = "/LinkedinPhoto.jpeg" alt = "A picture of Ayaan" width = "250" height = "250" /> 
+		<p> <b> <i> Socials </i> </b> </p>
+		<p> <a href="https://www.instagram.com/__ayaanali/" target= "_blank">
+			Instagram </a> </p>
+		<p> <a href="https://twitter.com/__ayaanali" target= "_blank">
+			Twitter </a> </p> 
+		<p> <a href="https://www.linkedin.com/in/ayaan-ali-089848208/" target= "_blank">
+			Linkedin </a> </p>
+		<p> <a href="https://www.columbiaspectator.com/contributors/Ayaan-Ali/" target= "_blank">
+			Columbia Spectator </a> </p>
+</center>
+</div> 
+</template> 
