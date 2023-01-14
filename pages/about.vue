ec3f924825b9c44f2b314d5f1378398bb9d48ba6
ayaan ali <ayaanali5204@outlook.com>
2023-01-14 10:09:57 -0500

    Last minute
    Minor changes

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
     <div id = "AboutMe"> 
     <center> 
         <h1> About me </h1> 
-        <img src = "LinkedinPhoto.jpeg" alt = "A picture of Ayaan" width = "250" height = "250" id="homepic"/>
+        <img src = "LinkedinPhoto.png" alt = "A picture of Ayaan" width = "250" height = "250" id="homepic"/>
 
         <p id = "bio"> Hi! I'm an aspiring developer from Queens, NYC currently studying computer science and mathematics @ Columbia University. 
             I'm progressively proficient in Java, Python, HTML5/CSS/Javascript, specifically the Vue and Nuxt JS frameworks. I'm also 
@@ -17,6 +17,8 @@
         <p> <span style = "color: lightgreen"> Spring 2023: </span> Data Structures & Algorithms (<span style = "color: #b5651d">Java</span>), Accelerated Multivariable Calculus (III-IV), Linear Algebra, Literature Humanities II, and University Writing: Contemporary Essays. </p>
         <p> <span style = "color: #C4A484"> Fall 2022: </span> Introduction to Computer Science and Programming (<span style = "color: #b5651d">Java</span>), Calculus II, Calculus-Based Introduction to Statistics, Literature Humanities I, and Frontiers of Science. </p>
         <p> <span style = "color: lightseagreen"> 2021-22:</span> AP Calculus AB, AP Computer Science Principles (<span style = "color: green">Python</span>).</p>
+
+        <p> Please feel free to follow me on any platforms and message me for any inquiry. </p>
         
     </center> 
      </div> 
@@ -24,6 +26,9 @@
 <style> 
     @import url('https://fonts.googleapis.com/css2?family=Anton&family=EB+Garamond&family=Fredoka+One&family=JetBrains+Mono:wght@500&display=swap'); 
 
+div { 
+    background-color: black;
+}
 #AboutMe { 
     background-color: black; 
     color: white; 
